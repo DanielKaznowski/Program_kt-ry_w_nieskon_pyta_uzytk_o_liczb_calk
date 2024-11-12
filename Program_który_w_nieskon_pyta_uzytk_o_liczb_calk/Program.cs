d2bd14a00b9715bf44368b004d9ca65a679c898f
Daniel_Kaznowski <Daniel_Kaznowski@MR_ROBOT>
2024-11-13 00:09:08 +0100

    Dodaj pliki projektów.

---
+++ Program_który_w_nieskon_pyta_uzytk_o_liczb_calk/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        while (true) // Pętla nieskończona
+        {
+            Console.Write("Podaj liczbę całkowitą: ");
+
+            // Odczytujemy i konwertujemy wprowadzoną wartość na liczbę całkowitą
+            if (int.TryParse(Console.ReadLine(), out int liczba))
+            {
+                if (liczba < 0) // Sprawdzamy, czy liczba jest mniejsza od zera
+                {
+                    Console.WriteLine("Wprowadzono liczbę mniejszą od zera. Kończenie programu.");
+                    break; // Opuszczenie pętli
+                }
+
+                Console.WriteLine("Wprowadzono liczbę: " + liczba);
+            }
+            else
+            {
+                Console.WriteLine("Nieprawidłowy format. Spróbuj ponownie.");
+            }
+        }
+    }
+}
